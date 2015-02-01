0600b8e09b77519f298cbf7614740cc9b24a5489
Junjie Kai <opendoublej@gmail.com>
2015-02-01 15:04:29 -0500

    Add GPIO Control feature

---
+++ src/io/github/jkai/main/logic/GpioControl.java
@@ -0,0 +1,44 @@
+package io.github.jkai.main.logic;
+
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.RaspiPin;
+
+public class GpioControl{
+	public void test1 () throws InterruptedException{
+		final GpioController gpio = GpioFactory.getInstance();
+
+		//set up pin#01,02 as output pins
+		final GpioPinDigitalOutput pin1 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01,"LED#1");
+		final GpioPinDigitalOutput pin2 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02,"LED#2");
+
+		//LED#1 turn on for 3s
+		pin1.high();
+
+		Thread.sleep(3000);
+
+		//LED#1 turn off
+		pin1.low();
+
+        //LED#2 turn on for 3s
+		pin2.high();
+
+		Thread.sleep(5000);
+
+		//LED#2 turn off
+		pin2.low();
+
+		//LED#1&2 turn on for 1s
+
+		pin1.high();
+		pin2.high();
+
+		Thread.sleep(1000);
+
+		pin1.low();
+		pin2.low();
+
+		gpio.shutdown();
+	}
+}
--- src/io/github/jkai/main/logic/Main.java
+++ src/io/github/jkai/main/logic/Main.java
@@ -3,8 +3,15 @@ package io.github.jkai.main.logic;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		final GpioControl gc = new GpioControl();
+		try {
+			gc.test1();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 	}
 
 }
